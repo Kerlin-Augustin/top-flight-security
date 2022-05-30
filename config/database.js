a24456eff8eb313212038ac0647c1fa2be22f4a6
Kerlin Augustin <kerlin.augustin1@gmail.com>
2022-05-30 05:27:59 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+// exporting whole object with a url and dbname property
+module.exports = {
+
+    // const url 'mongodb://127.0.0.1:27017/street-fighters'
+    'url' : 'mongodb+srv://kerlinaugustin:sneakyboi7@fullstack1.xmrfp.mongodb.net/fullstack1?retryWrites=true&w=majority', 
+    'dbName': 'fullstack1'
+};
